b8f0e65f36d32151384f031bc1e998d64e81870e
Ali asghar Ghanati <ali@Darkness.local>
2021-11-28 18:21:47 +0330

    come on

--- _nuxt/static/1638106615/statistical/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1638106615",layout:"default",error:a,state:{title:"Statistical model test"},serverRendered:true,routePath:"\u002Fstatistical",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
